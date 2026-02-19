0f464985fda11fdf11896d811ac5000c141bd693
adebabs-tec <ruqoyatomotayo@gmail.com>
2026-02-19 13:09:10 +0100

    using middleware

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const tasksRouter = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasksRouter);
+app.use(notFound);
 
 const port = 3000;
 

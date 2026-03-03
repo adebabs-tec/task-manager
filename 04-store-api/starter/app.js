7ffb2135d77a28ba870ebad6a714ac9587b625b9
adebabs-tec <ruqoyatomotayo@gmail.com>
2026-03-03 15:10:18 +0100

    contd the tutorial

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,29 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundHandlerMiddleware = require("./middleware/not-found");
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundHandlerMiddleware);
+
+app.get("/products", (req, res) => {
+  res.send("<h1>this is the products page</h1>");
+});
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connectDB
+    console.log(port, `Server is listening at port ${port}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
